a7af95ff07c4deae02bc5c9a1b5ab98fe2cd7556
bendevriesxd@gmail.com <bendevriesxd@gmail.com>
2023-04-12 11:56:58 +0200

    ranged attack blocked by elements but with more logical code

--- Shizen/Assets/Enemies/Scripts/EnemyHitsPlayer.cs
+++ Shizen/Assets/Enemies/Scripts/EnemyHitsPlayer.cs
@@ -8,9 +8,16 @@ public class EnemyHitsPlayer : MonoBehaviour
     public float damage = 5;
     public Transform player;
 
+    private int elementLayerIndex = 0;
+
+    private void Start()
+    {
+        elementLayerIndex = LayerMask.NameToLayer("Elements");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (gameObject.tag == "Projectile" && other.gameObject.layer == 3) Destroy(gameObject);
+        if (gameObject.tag == "Projectile" && other.gameObject.layer == elementLayerIndex) Destroy(gameObject);
 
         if (other.gameObject.tag == "Player" && !cooldown)
         {
