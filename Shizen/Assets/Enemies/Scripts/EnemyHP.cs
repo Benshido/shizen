0f091a7fd38219ad9d13637e39624501f575e4c8
bendevriesxd@gmail.com <bendevriesxd@gmail.com>
2023-04-13 20:43:47 +0200

    enemy health bar. Also raycast to wall lenght at 0 hopefully I don't break everything.

--- Shizen/Assets/Enemies/Scripts/EnemyHP.cs
+++ Shizen/Assets/Enemies/Scripts/EnemyHP.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHP : MonoBehaviour
 {
 
     [SerializeField] float maxHitPoints = 5;
+    public Slider hitPointsValue;
 
     private float hitPoints = 1;
     public float HitPoints { get { return hitPoints; } }
@@ -13,13 +15,15 @@ public class EnemyHP : MonoBehaviour
     void Start()
     {
         hitPoints = maxHitPoints;
-
+        hitPointsValue.maxValue = maxHitPoints;
+        hitPointsValue.value = hitPoints;
         IsAlive = true;
     }
 
     public void TakeAttack(float damage)
     {
         hitPoints -= damage;
+        hitPointsValue.value = hitPoints;
         if (hitPoints <= 0) IsAlive = false;
 
         if (gameObject.transform.parent != null && gameObject.transform.parent.gameObject.GetComponent<EnemyAIController>())
